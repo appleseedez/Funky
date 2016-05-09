96087734dce669ffb3531afaf865dda321016ad5
cjj <chenjianjun571@gmail.com>
2016-05-09 14:08:48 +0800

    酒店详情修改

--- components/hotel-details.jsx
+++ components/hotel-details.jsx
@@ -44,55 +44,56 @@ const HotelBaseInfo = React.createClass({
   render () {
     return (
       <div className="base-info">
-          <h1 className="mgb10">{this.props.name}</h1>
-          <div className="p mgb30 clearfix">
-              <p>规格类型<b>{this.props.typeName}</b></p>
-              <p>价格<span>¥<b>{this.props.lowestConsumption}</b>-<b>{this.props.highestConsumption}</b>/桌</span></p>
-            <p>场厅数量<span><b>{(this.props.banquetHall && this.props.banquetHall.length) || 0}</b>个专用宴会厅</span></p>
-              <p>最大容客数<span><b>{this.props.maxTableNum}</b>桌</span></p>
-              <p id="J_AddressButton" >所在地址:<span>
-                <a href={'/map/'+this.props.longitude+'/'+this.props.latitude} target='_blank'>
-                  <b>{this.props.address}</b>
-                </a>
-                <i className="ico-8-js" />
-                </span>
-              </p>
-          </div>
-          <div id='J_InfoContainer' className="score-info mgb40 clearfix">
-              <div className="star-box">
-                  <div className="star">
-                      <span>服务质量</span>
-                      <div>
-                          <i className="ico-star-3-js ico-star-3-gray-js" />
-                          <i className="ico-star-3-js ico-star-3-pink-js" style={{width: 45}} />
-                      </div>
-                  </div>
-                  <div className="star">
-                      <span>菜品质量</span>
-                      <div>
-                          <i className="ico-star-3-js ico-star-3-gray-js" />
-                          <i className="ico-star-3-js ico-star-3-pink-js" />
-                      </div>
-                  </div>
-                  <div className="star">
-                      <span>装修档次</span>
-                      <div>
-                          <i className="ico-star-3-js ico-star-3-gray-js" />
-                          <i className="ico-star-3-js ico-star-3-pink-js" />
-                      </div>
-                  </div>
+        <h1 className="mgb10">{this.props.name}</h1>
+        <div className="p mgb30 clearfix">
+          <p>酒店类型:<span><b>{this.props.typeName}</b></span></p>
+          <p>消费价格:<span><b>{'¥'+this.props.lowestConsumption}</b>-<b>{this.props.highestConsumption}</b>/桌</span></p>
+          <p>宴会厅数量:<span><b>{(this.props.banquetHall && this.props.banquetHall.length) || 0}</b>个专用宴会厅</span></p>
+          <p>最大容客数:<span><b>{this.props.maxTableNum}</b>桌</span></p>
+          <p>联系方式:<span><b>400-015-9999</b></span></p>
+          <p id="J_AddressButton" >酒店地址:<span>
+            <a href={'/map/'+this.props.longitude+'/'+this.props.latitude} target='_blank'>
+              <b>{this.props.address}</b>
+            </a>
+            <i className="ico-8-js" />
+            </span>
+          </p>
+        </div>
+        <div id='J_InfoContainer' className="score-info mgb40 clearfix">
+          <div className="star-box">
+            <div className="star">
+              <span>服务质量</span>
+              <div>
+                <i className="ico-star-3-js ico-star-3-gray-js" />
+                <i className="ico-star-3-js ico-star-3-pink-js" style={{width: 45}} />
               </div>
-              <div className="etc">
-                  <div className="item">
-                      <em>大礼包</em>
-                      <a href="/active/libao" target="_blank">通过金色百年预定婚宴，领取12999大礼包</a>
-                  </div>
-                  <div className="item">
-                      <em>组合优惠</em>
-                      <a href="/active/zuhe" target="_blank">消费项目越多，优惠力度越大</a>
-                  </div>
+            </div>
+            <div className="star">
+              <span>菜品质量</span>
+              <div>
+                <i className="ico-star-3-js ico-star-3-gray-js" />
+                <i className="ico-star-3-js ico-star-3-pink-js" />
               </div>
+            </div>
+            <div className="star">
+              <span>装修档次</span>
+              <div>
+                <i className="ico-star-3-js ico-star-3-gray-js" />
+                <i className="ico-star-3-js ico-star-3-pink-js" />
+              </div>
+            </div>
           </div>
+          <div className="etc">
+            <div className="item">
+              <em>大礼包</em>
+              <a href="/active/libao" target="_blank">通过金色百年预定婚宴，领取12999大礼包</a>
+            </div>
+            <div className="item">
+              <em>组合优惠</em>
+              <a href="/active/zuhe" target="_blank">消费项目越多，优惠力度越大</a>
+            </div>
+          </div>
+        </div>
       </div>
     )
   },
